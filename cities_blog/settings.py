6a60d07e5359eba7d396d9f7f6ed0cd4fa477a91
Jose-Contreras <104838758+JoseContrerasP@users.noreply.github.com>
2023-12-13 13:02:05 -0400

    delete staticfiles_dirs

--- cities_blog/settings.py
+++ cities_blog/settings.py
@@ -69,19 +69,15 @@ WSGI_APPLICATION = "cities_blog.wsgi.application"
 
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     "default": dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
         default="postgresql://postgres:postgres@localhost:5432/mysite",
         conn_max_age=600,
     )
 }
 
-
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -114,20 +110,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-STATICFILES_DIRS = [
-    os.path.join(
-        BASE_DIR,
-        "static",
-    )
-]
 
 LOGIN_URL = "signin"
 
